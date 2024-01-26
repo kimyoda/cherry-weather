624b79b61a1b8e091e57dad66ce0f6b97192476d
kimyohan <rladygks1210@naver.com>
2024-01-26 16:09:10 +0900

    first

---
+++ src/test/java/com/example/finalbootproject/FinalBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
